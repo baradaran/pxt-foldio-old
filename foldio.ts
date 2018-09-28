cc3330c6b435d5d639a510ddad4f41e51e02968d
Amir <amirhossein.baradaran@gmail.com>
2018-09-28 15:06:47 +0200

    debugging the syntax

--- foldio.ts
+++ foldio.ts
@@ -15,7 +15,7 @@ enum MyEnum {
  * Custom blocks
  */
 //% weight=100 color=#0fbc11 icon=""
-namespace custom {
+namespace foldio {
     /**
      * TODO: describe your function here
      * @param n describe parameter here, eg: 5
